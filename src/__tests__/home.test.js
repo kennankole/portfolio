70655b275cc99b4b90099ac29f82b748ee712f2c
kennankole <kennankole@gmail.com>
2023-02-13 11:37:27 +0300

    Fixing linter errors

--- src/__tests__/home.test.js
+++ src/__tests__/home.test.js
@@ -1,9 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import Enzyme, { shallow, render, mount } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Adapter from '@cfaester/enzyme-adapter-react-18';
 import Home from '../features/Home';
-import { wrap } from '@cfaester/enzyme-adapter-react-18/dist/enzyme-adapter-utils';
 
 Enzyme.configure({ adapter: new Adapter() });
 
