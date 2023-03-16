dec5e86fc0b192dafc7c0d44e43ba0af678372d9
kennankole <kennankole@gmail.com>
2023-03-16 21:08:56 +0300

    Positioning project items using Tailwind css

--- src/features/projects/ProjectCard.js
+++ src/features/projects/ProjectCard.js
@@ -1,13 +1,19 @@
 import cardData from '../../app/cardData';
 
 const ProjectCard = () => (
-  <div className="card-container">
+  <div className="flex flex-wrap justify-center">
     {Object.values(cardData).map((card) => (
-      <div key={card.title}>
-        <img src={card.image} alt={card.title} className="h-10 w-10 rounded-full" />
-        <h2>{card.title}</h2>
-        <p>{card.description}</p>
-        <p>{card.language}</p>
+      <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 m-1 border-2 border-sky-500" key={card.title}>
+        <div className="flex">
+          <div className="flex flex-col">
+            <div className="flex justify-center">
+              <img src={card.image} alt={card.title} className="h-10 w-10 rounded-full" />
+            </div>
+            <h2>{card.title}</h2>
+            <p>{card.description}</p>
+            <p>{card.language}</p>
+          </div>
+        </div>
       </div>
     ))}
   </div>
--- src/features/projects/Projects.js
+++ src/features/projects/Projects.js
@@ -1,12 +1,14 @@
 import ProjectCard from './ProjectCard';
 
 const Projects = () => (
-  <div>
-    <h1>Projects</h1>
-    <div className="project-container">
-      <h2>Things I have built so far</h2>
-      <ProjectCard />
+  <div className="p-10">
+    <div className="flex justify-center">
+      <div className="flex flex-col">
+        <h2 className="flex justify-center">Projects</h2>
+        <h1 className="flex justify-center">Things I have built so far</h1>
+      </div>
     </div>
+    <ProjectCard />
   </div>
 );
 export default Projects;
