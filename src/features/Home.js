66f4c71a50754bc507cb08744d24022712beb136
kennankole <kennankole@gmail.com>
2023-04-03 17:53:28 +0300

    Changed font style in the home page component

--- src/features/Home.js
+++ src/features/Home.js
@@ -2,7 +2,7 @@ import profile from '../images/profile-pic.png';
 
 const Home = () => (
   <main>
-    <div className="flex justify-around m-8 p-5 hello">
+    <div className="flex justify-around hello">
       <div className="flex items-center">
         <p className="font-bold max-[600px]:text-[15px] min-[600px]:text-[35px] min-[768px]:text-[40px]">
           Hi 👋,
@@ -17,8 +17,8 @@ const Home = () => (
           I build things for web
         </p>
       </div>
-      <div className="flex items-center p-5 justify-end min-[768px]:max-w-[100%] max-[600px]:max-w-[55%]">
-        <img src={profile} alt="profile-pic" className="max-w-[100%]" />
+      <div className="flex items-center p-10 justify-end min-[768px]:max-w-[100%] max-[600px]:max-w-[50%]">
+        <img src={profile} alt="profile-pic" className="max-[600px]:max-w-[70%] min-[600px]:max-w-[100%]" />
       </div>
     </div>
   </main>
